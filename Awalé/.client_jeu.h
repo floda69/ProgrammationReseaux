977ec624844c2b4f24d86c8d330cd91545d8e95b
floda69 <floflojourda@gmail.com>
2024-11-24 15:47:38 +0100

    delete old code

--- Awalé/.client_jeu.h
+++
@@ -1,6 +0,0 @@
-#ifndef CLIENT_JEU_H
-#define CLIENT_JEU_H
-
-// void deserialize_struct(char *buffer, Awale *jeu)
-
-#endif
--- Awalé/.serveur_jeu.h
+++
@@ -1,6 +0,0 @@
-#ifndef SERVEUR_JEU_H
-#define SERVEUR_JEU_H
-
-void serialize_struct(struct Awale *jeu, char *buffer);
-
-#endif
